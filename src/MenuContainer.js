7717cee311de15906efa778bc2e4c76fd679f327
Yasir Arafath <rony1382006@gmail.com>
2021-07-07 15:04:20 +0300

    Initial commit

---
+++ src/MenuContainer.js
@@ -0,0 +1,56 @@
+import React, {Component} from 'react';
+import MenuButton from './MenuButton';
+import Menu from './Menu';
+
+class MenuContainer extends Component{
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            visible: false
+        };
+
+        this.toggleMenu = this.toggleMenu.bind(this);
+        this.handleMouseDown = this.handleMouseDown.bind(this);
+    }
+
+    toggleMenu(){
+        this.setState({
+            visible: !this.state.visible
+        });
+    }
+
+    handleMouseDown(e){
+        this.toggleMenu();
+
+        console.log('Clicked');
+        e.stopPropagation();
+    }
+
+    render(){
+        return(
+            
+            <div>
+                <MenuButton handleMouseDown={this.handleMouseDown} />
+                <Menu handleMouseDown = {this.handleMouseDown}
+                      menuVisibility = {this.state.visible} />
+                <div>
+                    <p>Can you spot the item that doesn't belong?</p>
+                    <ul>
+                        <li>Lorem</li>
+                        <li>Ipsum</li>
+                        <li>Dolor</li>
+                        <li>Sit</li>
+                        <li>Bumblebees</li>
+                        <li>Aenean</li>
+                        <li>Consectetur</li>
+                        
+                    </ul>
+                </div>
+            </div>
+        )
+    }
+}
+
+export default MenuContainer;
